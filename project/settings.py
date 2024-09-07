ac39a71380c519b77ee04a7babad4fcc3cf99e44
Miquéias Silva <d.miqueias@academico.ifg.edu.br>
2024-09-07 19:03:34 -0300

    Heroku files statics

--- project/settings.py
+++ project/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Diretório onde os arquivos estáticos serão coletados
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
