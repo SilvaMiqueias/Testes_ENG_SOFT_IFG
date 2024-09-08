74239178a956e2b97348ffd7a5e350b342b383ed
Miquéias Silva <d.miqueias@academico.ifg.edu.br>
2024-09-07 23:26:43 -0300

    Configuração de login

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('crud.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    path('index/', auth_views.LogoutView.as_view(template_name='index.html'), name='home'),
+    path('teste/', auth_views.LogoutView.as_view(template_name='teste.html'), name='teste'),
 ]
