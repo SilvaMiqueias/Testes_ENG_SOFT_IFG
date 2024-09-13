b7a6ea116b10e466333ad5eae2b247f643dbbb19
Miquéias Silva <d.miqueias@academico.ifg.edu.br>
2024-09-13 12:11:39 -0300

    Atualização UI

--- pedido/views.py
+++ pedido/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Pedido
 from .forms import PedidoForm
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def pedido_list(request):
     pedidos = Pedido.objects.all()
     return render(request, 'pedido_list.html', {'pedidos': pedidos})
 
+@login_required
 def pedido_detail(request, pk):
     pedido = get_object_or_404(Pedido, pk=pk)
     return render(request, 'pedido_detail.html', {'pedido': pedido})
 
+@login_required
 def pedido_create(request):
     if request.method == 'POST':
         form = PedidoForm(request.POST)
@@ -20,6 +25,7 @@ def pedido_create(request):
         form = PedidoForm()
     return render(request, 'pedido_form.html', {'form': form})
 
+@login_required
 def pedido_update(request, pk):
     pedido = get_object_or_404(Pedido, pk=pk)
     if request.method == 'POST':
@@ -31,6 +37,7 @@ def pedido_update(request, pk):
         form = PedidoForm(instance=pedido)
     return render(request, 'pedido_form.html', {'form': form})
 
+@login_required
 def pedido_delete(request, pk):
     pedido = get_object_or_404(Pedido, pk=pk)
     if request.method == 'POST':
